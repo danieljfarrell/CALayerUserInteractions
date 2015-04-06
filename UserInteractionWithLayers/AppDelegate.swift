20b1e2ce801a5e68d2e860786098c9de23390878
daniel j farrell <daniel.james.farrell@gmail.com>
2015-04-06 13:00:01 +0100

    Initial Commit

---
+++ UserInteractionWithLayers/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  UserInteractionWithLayers
+//
+//  Created by Daniel Farrell on 06/04/2015.
+//  Copyright (c) 2015 Daniel Farrell. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
